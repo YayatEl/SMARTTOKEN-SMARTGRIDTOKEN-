edbb10d09b49b78e4d0eb60b12baee0a743d37cb
yayat <yayat.zero@gmail.com>
2022-05-07 16:32:04 +0800

    change the number

--- contracts/Smarttoken.sol
+++ contracts/Smarttoken.sol
@@ -2,7 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SmartToken is ERC20 {
-    constructor() ERC20("SmartToken", "SMT") {
+contract Smarttoken is ERC20 {
+
+    constructor () ERC20("Smarttoken", "SMT") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
